c76b93cab3c70f2a261bc4ce17108ed3a37bfffb
Kennedy Sovine <s.sovine@Me.com>
2025-07-08 10:21:24 +0100

    Hard coded Business

--- ravenous/src/components/Business/Business.js
+++ ravenous/src/components/Business/Business.js
@@ -1,20 +1,34 @@
+import React from 'react';
+import './Business.css';
+
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name: 'MarginOtto Pizzeria',
+    address: '1010 Paddington Way',
+    city: 'Flavortown',
+    state: 'NY',
+    zipCode: '10101',
+    category: 'Italian',
+    rating: 4.5,
+    reviewCount: 90
+};
+
+
 export const Business = () => {
     return (
-        <div className="business">
-            <img src="https://via.placeholder.com/150" alt="Business Logo" />
-            <h1>Name Placeholder</h1>
-            <div className="business-info">
-                <div className = "location">
-                    <p>Address</p>
-                    <p>City</p>
-                    <p>Sate, Zip Code</p>
+        <div className="Business">
+            <img className="Business-image" src={business.imageSrc} alt={business.name} />
+            <div className="Business-info">
+                <div className="Business-name">{business.name}</div>
+                <div className="Business-address">
+                    {business.address}<br />
+                    {business.city}<br />
+                    {business.state} {business.zipCode}
                 </div>
-                <div className="about">
-                    <h2>CATEGORY</h2>
-                    <p>Rating</p>
-                    <p>Review Count</p>
-                </div>
+                <span className="Business-category">{business.category}</span>
+                <span className="Business-rating">{business.rating} stars</span>
+                <span className="Business-reviews">{business.reviewCount} reviews</span>
             </div>
         </div>
-    )
+    );
 }
