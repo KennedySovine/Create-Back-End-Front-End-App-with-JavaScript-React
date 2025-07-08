dd1d715d2308e4c27dd64e995a5633b4db4bbcd7
Kennedy Sovine <s.sovine@Me.com>
2025-07-08 10:20:05 +0100

    Build Business Component

---
+++ ravenous/src/components/Business/Business.js
@@ -0,0 +1,20 @@
+export const Business = () => {
+    return (
+        <div className="business">
+            <img src="https://via.placeholder.com/150" alt="Business Logo" />
+            <h1>Name Placeholder</h1>
+            <div className="business-info">
+                <div className = "location">
+                    <p>Address</p>
+                    <p>City</p>
+                    <p>Sate, Zip Code</p>
+                </div>
+                <div className="about">
+                    <h2>CATEGORY</h2>
+                    <p>Rating</p>
+                    <p>Review Count</p>
+                </div>
+            </div>
+        </div>
+    )
+}
